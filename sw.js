090b98fac0885f68d3581a1c4a9b79f3a692d6ff
providence787 <providence.inc.787@gmail.com>
2026-09-14 08:45:42 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const V='sleeplog-v1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==V).map(x=>caches.delete(x))))
+    .then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin) return;            // GAS へのPOSTは触らない
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const cp=res.clone(); caches.open(V).then(c=>c.put(e.request,cp)); return res;
+  }).catch(()=>caches.match('./'))));
+});
