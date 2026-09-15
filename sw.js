3522d387a6b2c96c076de8f5f3aa7023adaa2944
providence787 <providence.inc.787@gmail.com>
2026-09-15 09:29:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='sleeplog-v1';
+const V='sleeplog-v2';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
